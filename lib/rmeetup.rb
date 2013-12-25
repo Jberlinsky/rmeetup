883c16c32e1ceabeeec00d4be4792d8f40b2f99d
Liam Morley <liam@carpeliam.com>
2013-12-25 01:07:28 -0500

    remove references to Echoe, replace with bundler
    This will allow the gem to be installed via `gem "rMeetup", github: "carpeliam/rmeetup"`.

--- lib/rmeetup.rb
+++ lib/rmeetup.rb
@@ -5,30 +5,31 @@ require 'json'
 require 'rmeetup/type'
 require 'rmeetup/collection'
 require 'rmeetup/fetcher'
+require 'rmeetup/version'
 
 module RMeetup
-  
+
   # RMeetup Errors
   class NotConfiguredError < StandardError
     def initialize
       super "Please provide your Meetup API key before fetching data."
     end
   end
-  
+
   class InvalidRequestTypeError < StandardError
     def initialize(type)
       super "Fetch type '#{type}' not a valid."
     end
   end
-  
+
   # == RMeetup::Client
-  # 
+  #
   # Essentially a simple wrapper to delegate requests to
   # different fetcher classes who are responsible for fetching
   # and parsing their own responses.
   class Client
     FETCH_TYPES = [:topics, :cities, :members, :rsvps, :events, :groups, :comments, :photos]
-    
+
     # Meetup API Key
     # Get one at http://www.meetup.com/meetup_api/key/
     # Needs to be the group organizers API Key
@@ -44,14 +45,14 @@ module RMeetup
     @@api_version = nil
     def self.api_version; @@api_version; end;
     def self.api_version=(version); @@api_version = version; end;
-    
+
     def self.fetch(type, options = {})
       check_configuration!
-      
+
       # Merge in all the standard options
       # Keeping whatever was passed in
       options = default_options.merge(options)
-      
+
       if FETCH_TYPES.include?(type.to_sym)
         # Get the custom fetcher used to manage options, api call to get a type of response
         fetcher = RMeetup::Fetcher.for(type, api_version)
@@ -60,14 +61,14 @@ module RMeetup
         raise InvalidRequestTypeError.new(type)
       end
     end
-    
+
     protected
       def self.default_options
         {
           :key => api_key
         }
       end
-      
+
       # Raise an error if RMeetup has not been
       # provided with an api key
       def self.check_configuration!
