4c5e8c999de54070195c74610cd1d213314e7fa5
Ronald Brachetti <ronald.brachetti@gmail.com>
2016-07-12 17:42:45 +0200

    add v2 groups

--- lib/rmeetup/fetcher/groups.rb
+++ lib/rmeetup/fetcher/groups.rb
@@ -1,6 +1,7 @@
 module RMeetup
   module Fetcher
     class Groups < Base
+      MAX_API_VERSION = 2
       def initialize(api_version = nil)
         super(api_version) # no support for API versioning yet
         @type = :groups
@@ -8,7 +9,11 @@ module RMeetup
       
       # Turn the result hash into a Group Class
       def format_result(result)
-        RMeetup::Type::V1::Group.new(result)
+        if @api_version == 1
+          RMeetup::Type::V1::Group.new(result)
+        else
+          RMeetup::Type::V2::Group.new(result)
+        end
       end
     end
   end
