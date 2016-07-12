9f47cd46925e1d71fbd384a4edb34e676fc60953
Ronald Brachetti <ronald.brachetti@gmail.com>
2016-07-12 18:18:33 +0200

    use version 3 api for groups

--- lib/rmeetup/fetcher/groups.rb
+++ lib/rmeetup/fetcher/groups.rb
@@ -1,7 +1,7 @@
 module RMeetup
   module Fetcher
     class Groups < Base
-      MAX_API_VERSION = 2
+      MAX_API_VERSION = 3
       def initialize(api_version = nil)
         super(api_version) # no support for API versioning yet
         @type = :groups
@@ -9,10 +9,15 @@ module RMeetup
       
       # Turn the result hash into a Group Class
       def format_result(result)
-        if @api_version == 1
-          RMeetup::Type::V1::Group.new(result)
-        else
-          RMeetup::Type::V2::Group.new(result)
+        case @api_version
+          when 1
+            RMeetup::Type::V1::Group.new(result)
+          when 2
+            RMeetup::Type::V2::Group.new(result)
+          when 3
+            RMeetup::Type::V3::Group.new(result)
+          else
+            RMeetup::Type::V1::Group.new(result)
         end
       end
     end
---
+++ lib/rmeetup/type/v3/group.rb
@@ -0,0 +1,39 @@
+module RMeetup
+  module Type
+    module V3
+
+      # == RMeetup::Type::V3::Group
+      #
+      # Data wrapper for a Group fetching response
+      # Used to access result attributes as well
+      # as programatically fetch relative data types
+      # based on this event.
+      
+      # See http://www.meetup.com/meetup_api/docs/3/groups/ for available fields
+
+      class Group < RMeetup::Type::Base
+
+        def event
+          @obj
+        end
+
+        # Special accessors that need typecasting or other parsing
+        def created
+          datetime_for 'created'
+        end
+        def updated
+          datetime_for 'updated'
+        end
+        def time
+          datetime_for 'time'
+        end
+
+        private
+        def datetime_for(field)
+          offset = Rational(event['utc_offset'], 24 * 60 * 60 * 1000) # hours * minutes * seconds * milliseconds
+          Time.at(event[field] / 1000).to_datetime.new_offset(offset)
+        end
+      end
+    end
+  end
+end
